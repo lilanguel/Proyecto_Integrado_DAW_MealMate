0fb1540edac5f978c17010ac297763dc2192dd50
Angel Alberto De los rios Navarro <angela.de.external@atos.net>
2023-05-20 00:41:12 +0200

    Creacion interfaces

---
+++ src/MealMate/src/app/interfaces/comida.interface.ts
@@ -0,0 +1,10 @@
+export interface Comida {
+  _id: string;
+  nombre: string;
+  calorias: number;
+  proteinas: number;
+  carbohidratos: number;
+  grasas: number;
+  fibras: number;
+  horario: string;
+}
---
+++ src/MealMate/src/app/interfaces/user.interface.ts
@@ -0,0 +1,10 @@
+export interface User {
+  _id: string;
+  nombre_usuario: string;
+  email: string;
+  sexo: string;
+  fecha_nacimiento: Date;
+  password: string;
+  peso: number;
+  altura: string;
+}
